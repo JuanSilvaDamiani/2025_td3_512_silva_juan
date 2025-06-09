7547465d133e3cd17d8c256a3d4072e77222f788
JuanSilvaDamiani <jsilva.arelec@gmail.com>
2025-06-09 00:06:11 -0300

    SegunDa consigna de Tp2

--- 3_trabajos_practicos/tp2/firmware/firmware.c
+++ 3_trabajos_practicos/tp2/firmware/firmware.c
@@ -1,139 +1,118 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
+#include "hardware/irq.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
 
-// Declaración de la cola para enviar las temperatura desde una tarea a otra
-// La cola se usará para enviar valores de tipo float (temperatura en grados Celsius)
-// QueueHandle_t es un tipo definido en FreeRTOS para manejar colas.
-QueueHandle_t temperatura_queue;
+/* Declaración de una cola de FreeRTOS para enviar datos del ADC desde una interrupción a una tarea.  
+QueueHandle_t es el "manejador de cola"
+cola_datos_adc es una variable global que guarda el "handle" de la cola que va a transportar los valores leídos desde el ADC  */ 
+QueueHandle_t cola_datos_adc;
 
 /**
- * Tarea que lee la temperatura del sensor interno del RP2040
- * y envía el valor en grados Celsius a través de una cola.
+ * Tarea que dispara la conversión ADC periódicamente.
  */
-void leotemp_task(void *pvParameters) {
-    // Inicialización del módulo ADC
+void tarea_dispara_adc(void *pvParameters) {
     adc_init();
+    adc_gpio_init(26); /* Habilita el GPIO26 que es uno de los 3 canales internos, 
+    para funcionar como entrada analógica para el canal 0 del ADC*/
+    adc_select_input(0); // Le indica al ADC que la próxima conversión se haga sobre el canal 0
+    
+  // Configura el FIFO del ADC
+  /*El ADC tiene buffer de datos (FIFO) donde guarda los resultados de las conversiones. Asi se pueden leer los resultados más tarde que es bueno para trabajar con interrupciones.
+La Sintaxis y parámetros son:
+adc_fifo_setup(
+    bool en,                // Habilita o deshabilita el FIFO
+    bool dreq_en,           // Genera señal DREQ cuando FIFO no está vacío (para DMA)
+    uint8_t dreq_thresh,    // Umbral de elementos en FIFO para activar DREQ
+    bool err_in_fifo,       // Si true, errores van al FIFO también
+    bool byte_shift         // Si true, resultado se alinea a 8 bits (descarta LSBs)
+);
+*/
+    adc_fifo_setup(
+        true,    // Habilitar FIFO
+        false,   // No usar DREQ
+        1,       // Umbral para DREQ (no importa aquí)
+        false,   // No incluir errores en el FIFO
+        false    // No desplazar bits (usar resolución completa)
+    );
 
-    // Habilita el sensor de temperatura interno del RP2040.
-    // Por defecto está deshabilitado para ahorrar energía.
-    adc_set_temp_sensor_enabled(true);
+    adc_fifo_drain();         // Limpia cualquier dato previo en el FIFO
+    adc_irq_clear();          // Limpia bandera de interrupción
+    adc_irq_set_enabled(true); // Habilita la interrupción del ADC
 
     while (true) {
-        // adc_select_input(4):
-        // Función del SDK de Raspberry Pi Pico que selecciona cuál de los 
-        //5 canales de entrada ADC (convertidor analógico a digital)
-        // del Pico 2 voy a usar. Para activar el canal del sensor de
-        // temperatura interno es el 4 y no está conectado a una pata externa del chip.
-        // NB: antes de esto hay que activar el sensor de temperatura con el adc_set_temp_sensor_enabled(true);
-        adc_select_input(4);
-
-        // adc_read():  lee el valor analógico del canal ADC activo, en este caso el 4 
-        // que es el sensor de temperatura interno del RP2040
-        // que genera un voltaje que varía linealmente con la temperatura y a una 
-        // temperatura de 27 °C, es de 0.706 V; y lo devuelve convertido
-        // en un número digital mediante el convertidor ADC en  un número entre 0 y 4095.
-        //Desde ahí vamos a usar la fórmula empírica 
-        //   V(Volt)= valor_adc × (3.3 / 4095)
-        //   V(Volt)= 0.706 V − 0.001721 × (T(°C) − 27 °C)  → despejando:
-        //   T(°C) = 27 °C – ((V(Volt) - 0.706 V) / 0.001721)
-        //
-        // uint16_t :  Es un "entero sin signo de 16 bits" (rango: 0 a 65535), nuestro valor máximo será 4095,
-        // porque el ADC del RP2040 tiene resolución de 12 bits y 2¹² = 4096
-        // valor_adc : Nombre de la variable que guarda el valor de conversión leído desde el ADC.
-        // Siendo que el 0 corresponde a 0.0 V y el 4095 corresponde a 3.3 V que es el voltaje de referencia,
-        // y luego como 0.706 V corresponde a 27 °C porque es una referencia fija general del RP2040
-        uint16_t valor_adc = adc_read();
-
-        // Convierto valor digital a voltaje real
-        float voltaje = valor_adc * 3.3f / 4095.0f;
-
-        // Convierto voltaje a temperatura en grados Celsius
-        float temperatura = 27.0f - (voltaje - 0.706f) / 0.001721f;
-
-        // Envía el valor de la temperatura (tipo float) a la cola temperatura_queue. 
-        //Luego, otra tarea puede recibirla usando xQueueReceive y hacer algo con ella
-        /*
-        Explicación:
-        temperatura_queue
-        Es una cola creada con xQueueCreate, donde se almacenan valores de tipo float (temperatura en grados Celsius en este caso). Las colas se usan para comunicar tareas entre sí en FreeRTOS.
-        temperatura
-        Es una variable float que contiene el valor de la temperatura calculada a partir de la lectura del sensor interno.
-        &temperatura
-        Se pasa la dirección de memoria de la variable temperatura. Esto se hace porque xQueueSend copia desde esa dirección el valor que se quiere poner en la cola.
-        portMAX_DELAY
-        Especifica cuánto tiempo debe esperar la función si la cola está llena. En este caso, portMAX_DELAY indica que debe esperar indefinidamente hasta que haya espacio disponible en la cola.
+        adc_run(true);                      // Habilita el ADC
+        adc_hw->cs |= ADC_CS_START_ONCE_BITS;  // Inicia una conversión
+        /*Forma directa de manipular los registros del hardware del ADC del RP2040. 
+        El Raspberry Pi Pico define esas estructuras y macros para facilitar el acceso directo al hardware.
+        adc_hw : es un puntero a la estructura que mapea los registros del hardware del ADC del RP2040
+        cs : es el registro de control y estado del ADC en el hardware del RP2040
+        ADC_CS_START_ONCE_BITS : es una máscara con el bit que el micro RP2040 reconoce para iniciar la 
+        conversión de forma puntual
         */
-        xQueueSend(temperatura_queue, &temperatura, portMAX_DELAY);
-
-        // Espero 2 segundos antes de la siguiente lectura.
-        // El retardo de 2 segundos se cuenta después de que se logró enviar a la cola. 
-        // Si la cola está llena, la tarea se queda esperando indefinidamente en xQueueSend(...) 
-        // y no empieza el vTaskDelay hasta que se libera espacio en la cola (es decir, 
-        // cuando otra tarea recibe y saca un elemento con xQueueReceive), el xQueueSend termina con éxito.
-        vTaskDelay(pdMS_TO_TICKS(2000));
+        vTaskDelay(pdMS_TO_TICKS(1000));   // Espera 1 segundo
     }
 }
 
 /**
- * Tarea que recibe la temperatura por cola y la muestra por consola.
- * Defino la tarea llamada printtemp_task. Como todas las tareas,
- *  recibe un parámetro genérico (void *pvParameters) que aquí no se usa.
+ * Tarea que recibe datos del ADC por cola y los imprime.
  */
-void printtemp_task(void *pvParameters) {
-//Declara una variable local para almacenar la temperatura que va a recibir.
-    float temp_recibida;
-    while (true) {
-        // Espero indefinidamente a recibir temperatura por cola
-/*
-•	xQueueReceive(temperatura_queue, &temp_recibida, portMAX_DELAY):
-Aquí la tarea espera a recibir un dato (un valor de temperatura tipo float) 
-desde la cola llamada temperatura_queue.
-o	temperatura_queue es una estructura propia de FreeRTOS para
- comunicación entre tareas.
-o	&temp_recibida es la dirección donde se almacenará el valor recibido.
-o	portMAX_DELAY indica que la tarea esperará indefinidamente
- hasta que haya un dato disponible.
-o	La función devuelve pdTRUE si recibió exitosamente un dato.
-•	if (...) { printf(...); }:
-Cuando recibe un dato, lo imprime por consola en formato de punto flotante
- con dos decimales, mostrando la temperatura en grados Celsius.
-En resumen: Esta tarea está dedicada a recibir continuamente
- valores de temperatura que otra tarea genera y envía por cola,
-  y a mostrarlos por consola.
-  Está sincronizada con la tarea lectora a través de la cola,
-   funcionando de forma cooperativa y sin bloquear el sistema.
- */
+void tarea_lee_adc(void *pvParameters) {
+    uint16_t valor_recibido;
 
-        if (xQueueReceive(temperatura_queue, &temp_recibida, portMAX_DELAY) == pdTRUE) {
-            printf("Temperatura interna: %.2f °C\n", temp_recibida);
+    while (true) {
+        if (xQueueReceive(cola_datos_adc, &valor_recibido, portMAX_DELAY) == pdTRUE) {
+            printf("Valor ADC recibido: %u\n", valor_recibido);
         }
     }
 }
 
 /**
- * Función principal que crea las tareas y arranca FreeRTOS.
+ * Manejadora de la interrupción (ISR) para el ADC. 
+ * Es una función que se ejecuta automáticamente cuando ocurre la interrupción que genera el ADC 
+ * cuando termina una conversión y pone un dato nuevo en su FIFO.
+ * La ISR detiene el flujo normal del programa para atender el evento.
  */
-int main() {
-    // Inicializo salida estándar (USB o UART según configuración de SDK)
-    stdio_init_all();
+void adc_isr() {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE; 
+    /* Intenta enviar un dato a la cola desde la ISR.
+    Si con eso se desbloquea una tarea de mayor prioridad, xHigherPriorityTaskWoken se pone en pdTRUE.
+    En definitiva le dice al sistema operativo si debe cambiar a otra tarea inmediatamente 
+    después de salir de la interrupción.
+    Luego, portYIELD_FROM_ISR() fuerza un cambio inmediato de contexto si esa bandera es pdTRUE.
+    */
 
-    // Creo la cola para un elemento tipo float
-    temperatura_queue = xQueueCreate(1, sizeof(float));
+    if (adc_fifo_get_level() > 0) {
+        uint16_t valor_adc = adc_fifo_get(); // Leo el valor del ADC desde el FIFO si es > 0
+        xQueueSendFromISR(cola_datos_adc, &valor_adc, &xHigherPriorityTaskWoken);// Envío el valor a la cola desde la ISR
+    }
 
-    // Creo la tarea que lee el ADC y manda por cola
-    xTaskCreate(leotemp_task, "LecturaTemp", 256, NULL, 1, NULL);
-
-    // Creo la tarea que recibe por cola e imprime por consola
-    xTaskCreate(printtemp_task, "ImprimirTemp", 256, NULL, 1, NULL);
-
-    // Inicio el scheduler de FreeRTOS
-    vTaskStartScheduler();
-
-    // Nunca debería llegar aquí
-    while (1);
+    adc_irq_clear(); // Limpio la bandera de interrupción
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);// Cambia de contexto si hace falta
 }
 
+/**
+ * Función principal.
+ */
+int main() {
+    stdio_init_all();
+
+    // Crear la cola
+    cola_datos_adc = xQueueCreate(1, sizeof(uint16_t));
+
+    // Configuro la interrupción del ADC
+    irq_set_exclusive_handler(ADC_IRQ_FIFO, adc_isr);
+    irq_set_enabled(ADC_IRQ_FIFO, true);
+
+    // Creo tareas
+    xTaskCreate(tarea_dispara_adc, "DisparoADC", 256, NULL, 1, NULL);
+    xTaskCreate(tarea_lee_adc, "LecturaADC", 256, NULL, 1, NULL);
+
+    // Inicio el scheduler
+    vTaskStartScheduler();
+
+    while (1); // Nunca debería llegar acá
+}
